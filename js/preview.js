98113e64570ac6e71298a416a67227331ea86637
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-30 19:46:21 +0300

    Убрал из обекта переменную pictureTemplate и функцию getUserPhotos в связи с тем что они используются только внутри preview.js

--- js/preview.js
+++ js/preview.js
@@ -4,16 +4,6 @@
   window.preview = {
     galleryOverlay: document.querySelector('.gallery-overlay'),
     similarPictureElement: document.querySelector('.pictures'),
-    pictureTemplate: document.querySelector('#picture-template').content,
-
-    getUserPhotos: function (number) {
-      return {
-        url: 'photos/' + number + '.jpg',
-        likes: window.utils.getRandomNumber(15, 200),
-        comments: getRandomNumberComments()
-      };
-    },
-
     getRenderPhotos: function () {
       var photoLength = photoGallery.length;
       var fragment = document.createDocumentFragment();
@@ -22,9 +12,15 @@
       }
       return fragment;
     }
-
   };
-
+  var pictureTemplate = document.querySelector('#picture-template').content;
+  var getUserPhotos = function (number) {
+    return {
+      url: 'photos/' + number + '.jpg',
+      likes: window.utils.getRandomNumber(15, 200),
+      comments: getRandomNumberComments()
+    };
+  };
   var getRandomNumberComments = function () {
     var temporaryComments = [];
     var commentsNumber = window.utils.getRandomNumber(0, 50);
@@ -36,13 +32,13 @@
   var getArrayPictures = function () {
     var photoGallery = [];
     for (var i = 0; i <= 25; i++) {
-      photoGallery[i] = window.preview.getUserPhotos(i + 1);
+      photoGallery[i] = getUserPhotos(i + 1);
     }
     return photoGallery;
   };
   var photoGallery = getArrayPictures();
   var getRenderPictures = function (photo) {
-    var photoElement = window.preview.pictureTemplate.cloneNode(true);
+    var photoElement = pictureTemplate.cloneNode(true);
     photoElement.querySelector('img').src = photo.url;
     photoElement.querySelector('.picture-comments').textContent = photo.comments.length;
     photoElement.querySelector('.picture-likes').textContent = photo.likes;
