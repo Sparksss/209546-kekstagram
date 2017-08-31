b160c6427c2b52ee510aade5ddd9340762bd180d
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-08-31 19:06:41 +0300

    Исправление

--- js/data.js
+++ js/data.js
@@ -1,6 +1,14 @@
 'use strict';
 
 (function () {
+  var COMMENTS = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+  ];
   window.collectionData = {
     getUserPhotos: function (number) {
       return {
@@ -13,7 +21,7 @@
       var temporaryComments = [];
       var commentsNumber = window.utils.getRandomNumber(0, 50);
       for (var i = 0; i < commentsNumber; i++) {
-        temporaryComments[i] = window.utils.COMMENTS[window.utils.getRandomNumber(0, 5)];
+        temporaryComments[i] = COMMENTS[window.utils.getRandomNumber(0, 5)];
       }
       return temporaryComments;
     }
