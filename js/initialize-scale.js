b1596263f3071f385fb1a8611a57ac5b00c1fbbe
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-09-06 15:41:45 +0300

    вынес все в отдельный файл

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -2,22 +2,39 @@
 
 (function () {
 
+  var MIN_VALUE = 25;
+
+  var MAX_VALUE = 100;
 
   var reduceImageSize = 'upload-resize-controls-button-dec';
 
   var increaseImageSize = 'upload-resize-controls-button-inc';
 
+  var resizeControls = document.querySelector('.upload-resize-controls');
+
+  var scaleElement = resizeControls.querySelector('.upload-resize-controls-value');
+
+  var pictureElement = document.querySelector('.effect-image-preview');
+
   // функция изменения масштаба изображения
 
-
-  window.initializeScale = function (scaleElement, callback) {
-    scaleElement.addEventListener('click', function (evt) {
-      var target = evt.target;
-      if (target.classList.contains(reduceImageSize)) {
-        callback(-1);
-      } else if (target.classList.contains(increaseImageSize)) {
-        callback(1);
-      }
-    });
+  var adjustScale = function (newValue) {
+    pictureElement.style.transform = 'scale(' + newValue / 100 + ')';
   };
+  window.initializeScale = function (controlSizeImage, callback, direction) {
+    var newValue = parseInt(controlSizeImage.value, 10) + 25 * direction;
+    if (newValue >= MIN_VALUE && newValue <= MAX_VALUE) {
+      controlSizeImage.value = newValue + '%';
+      callback(newValue);
+    }
+  };
+  // обработчик события для изменения размера фотографии
+  resizeControls.addEventListener('click', function (evt) {
+    var target = evt.target;
+    if (target.classList.contains(reduceImageSize)) {
+      window.initializeScale(scaleElement, adjustScale, -1);
+    } else if (target.classList.contains(increaseImageSize)) {
+      window.initializeScale(scaleElement, adjustScale, 1);
+    }
+  });
 })();
