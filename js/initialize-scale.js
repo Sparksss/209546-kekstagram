8703f42184aa504270e779a3ee7152b2f91ccb0d
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-09-06 22:57:24 +0300

    Наведение красоты в модуле

--- js/initialize-scale.js
+++ js/initialize-scale.js
@@ -1,21 +1,12 @@
 'use strict';
 
 (function () {
-
-  var reduceImageSize = 'upload-resize-controls-button-dec';
-
-  var increaseImageSize = 'upload-resize-controls-button-inc';
-
-  var resizeControls = document.querySelector('.upload-resize-controls');
-
   window.initializeScale = function (processedElement, callback) {
-    resizeControls.addEventListener('click', function (evt) {
-      var target = evt.target;
-      if (target.classList.contains(reduceImageSize)) {
-        callback(processedElement, -1);
-      } else if (target.classList.contains(increaseImageSize)) {
-        callback(processedElement, 1);
-      }
+    document.querySelector('.upload-resize-controls-button-dec').addEventListener('click', function () {
+      callback(processedElement, -1);
+    });
+    document.querySelector('.upload-resize-controls-button-inc').addEventListener('click', function () {
+      callback(processedElement, 1);
     });
   };
 })();
