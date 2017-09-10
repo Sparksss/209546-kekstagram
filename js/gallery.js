f8b390fb6a5dbd9be0b5516c786915f54d554a17
Ilya Rogatkin <toch.in.go@googlemail.com>
2017-09-11 00:20:42 +0300

    Удалили событие по клику, добавил по change,+ распознавание выполнено по value у input

--- js/gallery.js
+++ js/gallery.js
@@ -15,29 +15,29 @@
       }
     });
     filters.classList.remove(window.utils.CLASS_HIDDEN);
-    filters.querySelector('#filter-popular').addEventListener('click', function () {
-      photoCollection.sort(function (left, right) {
-        if (left.likes < right.likes) {
-          return 1;
-        } else if (left.likes > right.likes) {
-          return -1;
-        } else {
-          return 0;
-        }
-      });
-      similarPictureElement.innerHTML = '';
-      similarPictureElement.appendChild(window.pictures.getRenderPhotos(photoCollection));
-    });
-    filters.querySelector('#filter-discussed').addEventListener('click', function () {
-      photoCollection.sort(function (left, right) {
-        if (left.comments.length < right.comments.length) {
-          return 1;
-        } else if (left.comments.length > right.comments.length) {
-          return -1;
-        } else {
-          return 0;
-        }
-      });
+    filters.addEventListener('change', function (evt) {
+      if (evt.target.value === 'popular') {
+        photoCollection.sort(function (left, right) {
+          if (left.likes < right.likes) {
+            return 1;
+          } else if (left.likes > right.likes) {
+            return -1;
+          } else {
+            return 0;
+          }
+
+        });
+      } else if (evt.target.value === 'discussed') {
+        photoCollection.sort(function (left, right) {
+          if (left.comments.length < right.comments.length) {
+            return 1;
+          } else if (left.comments.length > right.comments.length) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+      }
       similarPictureElement.innerHTML = '';
       similarPictureElement.appendChild(window.pictures.getRenderPhotos(photoCollection));
     });
